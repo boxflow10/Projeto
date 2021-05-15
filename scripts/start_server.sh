34f79e6ad25f68917f981fc9f49d8bf268266cba
box <boxflow@protonmail.com>
2021-05-15 19:31:34 -0300

    Preparando para aws

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+cd /home/ubuntu/Projeto
+source env/bin/activate
 python manage.py runserver &
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+pkill -f runserver
+lsof -t -i tcp:8000 | xargs kill -9
