cd90afa8dc9436fbcc49c7270e85582fed97dc40
box <boxflow@protonmail.com>
2021-05-15 18:41:56 -0300

    Preparando para aws

--- scripts/dependencias.sh
+++ scripts/dependencias.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo apt-get -y install python3-venv virtualenv libpq-dev python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev build-essential gcc libpq-dev
 virtualenv env
 source env/bin/activate
