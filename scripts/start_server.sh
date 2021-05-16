0ec4c9bf0b762301f7a6c9211c455f52252db0a7
box <boxflow@protonmail.com>
2021-05-16 00:07:04 -0300

    Preparando para aws

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-cd /home/ubuntu/
-source env/bin/activate
-cd Projeto
-python manage.py runserver 0.0.0.0:8000 &
+cd /home/ubuntu/;source env/bin/activate
+cd /home/ubuntu/Projeto;python manage.py runserver 0.0.0.0:8000 &
